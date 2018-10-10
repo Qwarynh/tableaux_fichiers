df838b2997c794de50ad1dbc61ef0e0c0dd4af6a
Euphoria <eupho.42@gmail.com>
2018-10-10 09:59:57 +0200

    Premier commit cours

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
+using namespace std;
 
-int main(){
+int main()
+{
+vector <int> tab(3);
+tab[0]=3;
+tab[1]=4;
+tab[2]=5;
+tab.push_back(1000);
 
+cout<< tab[0] << " " << tab[1] << " " << tab[2] << " " << tab[3] << endl;
+cout << tab.size() << endl;
 
+tab[3]= 101;
+tab.erase(tab.begin() +1);
+cout << tab.size() << endl;
 return 0;
 }
